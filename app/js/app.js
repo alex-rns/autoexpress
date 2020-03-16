217a5ed3798c96b09cf6521406b0e745f4900b79
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2020-03-16 23:11:36 +0200

    Add slow scrolling to section(only tag "a")

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
 // autoclose mobile menu on click to link
-  $(".navbar-collapse a").click(function() {
+  $(".navbar-collapse a, .navbar-brand").click(function() {
     if (!$(this).hasClass("dropdown-toggle")) {
       $(".navbar-collapse").collapse('hide');
+    }
+  });
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
     }
   });
 
